d878289096835c8af7d04f8dee7be24292ad0fc2
g3tl0st <51173961+g3tl0st@users.noreply.github.com>
2022-12-10 14:04:01 +0700

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,57 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { loadGetInitialProps } from 'next/dist/shared/lib/utils';
 
 export default function Home() {
-  const [count, setCount] = useState(0);
+  const [input, setInput] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  function checkIfDuplicateExists(arr) {
+    return new Set(arr.map((v) => v.todo));
+  }
+
+  let isDuplicate = false;
+
+  function addTodo() {
+    // if input is empty then return
+    if (!input.replaceAll(' ', '')) return;
+
+    if (isDuplicate) return;
+
+    const id = todoList.length + 1;
+    setTodoList((prev) => [
+      ...prev,
+      {
+        id: id,
+        todo: input,
+        isCompleted: false,
+      },
+    ]);
+
+    setInput('');
+  }
+
+  function removeTodo(value) {
+    const index = todoList.indexOf(value);
+    console.log(index);
+
+    // if (index > -1) {
+    //   todoList.splice(index, 1);
+    // }
+    // setTodoList(todoList);
+  }
+
+  useEffect(() => {
+    let duplicate = checkIfDuplicateExists(todoList);
+
+    isDuplicate = duplicate.size < todoList.length;
+
+    if (isDuplicate) {
+      alert('Task is already exists');
+      return;
+    }
+  }, [todoList]);
 
   return (
     <div className={styles.container}>
@@ -11,21 +59,25 @@ export default function Home() {
         <title>Create Next App</title>
       </Head>
 
-      <main className={styles.main}>
+      <main>
         <div>TODO LIST</div>
+
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <button onClick={() => addTodo()}>Add</button>
+
         <div>
-          <span>
-            <input name="hi" />
-            <button>add</button>
-          </span>
-
-          <p>You clicked {count} times</p>
-          <button onClick={() => setCount(count + 1)}>Click me</button>
+          <ul>
+            {todoList.map((todo) => {
+              return (
+                <li key={todo.id}>
+                  {todo.todo}
+                  <button onClick={removeTodo(todo.id)}>remove</button>
+                </li>
+              );
+            })}
+          </ul>
         </div>
-        <span></span>
       </main>
-
-      <footer className={styles.footer}></footer>
     </div>
   );
 }
